ac40c767130bfdaf11a2b45521857bab0821172a
PaongInternational <bypaongpinew@gmail.com>
2025-10-30 19:13:06 +0700

    ☕kalau mau plugins dan update lainya, ke sini  https://whatsapp.com/channel/0029Vb6cgi6LSmbec90kZv02

---
+++ frontend/src/pages/Terminal.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+export default function Terminal(){
+  return (<div>
+    <h2>Terminal & Editor</h2>
+    <div className='card'>Monaco editor and xterm integration placeholders.</div>
+  </div>)
+}
